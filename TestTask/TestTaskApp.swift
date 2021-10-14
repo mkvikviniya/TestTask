58a5989c8840da327b362fdf5f729789a98a54b3
Mikhail Kvikviniya <mkvikviniya@icloud.com>
2021-10-14 16:55:10 +0300

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Михаил Квиквиния on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
